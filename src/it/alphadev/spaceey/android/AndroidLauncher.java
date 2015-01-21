09d8cb471aba30eabfc2c15b457d12111309ce62
Mercury5A2014 <binotturaja@gmail.com>
2015-01-21 13:33:13 +0100

    Initial commit

---
+++ src/it/alphadev/spaceey/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package it.alphadev.spaceey.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import it.alphadev.spaceey.Spaceey;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Spaceey(), config);
+	}
+}
