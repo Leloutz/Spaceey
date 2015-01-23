b28d492890f38c4304b09dd0992baa16e68147e6
Mercury5A2014 <binotturaja@gmail.com>
2015-01-23 21:49:37 +0100

    Aggiunta SplashActivity con fadein

--- src/it/alphadev/spaceey/android/AndroidLauncher.java
+++ src/it/alphadev/spaceey/android/AndroidLauncher.java
@@ -7,10 +7,21 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import it.alphadev.spaceey.Spaceey;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
+
+    @Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Spaceey(), config);
+
+       /* new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              //  Intent i = new Intent(AndroidLauncher.this, MainActivity.class);
+                //startActivity(i);
+                finish();
+            }
+        }, 3000);
+        */
 	}
 }
